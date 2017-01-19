1c24f1ef3f8eda4e8405416a20982b1b1696df11
WyrnCael <juanjpri@ucm.es>
2017-01-19 21:59:36 +0100

    Insertar Curso

--- DAO.js
+++ DAO.js
@@ -13,3 +13,52 @@ var pool = mysql.createPool({
     database: config.dbName
 });
 
+function insertCurso(datosCurso, callback){
+    pool.getConnection(function(err, con) {
+    if (err) {
+        callback(err);
+    } else {
+        con.query("INSERT INTO Cursos(Titulo, Descripcion, Localidad, Direccion, NumPlazas, FechaInicio, FechaFin)" + 
+                       " VALUES (?, ?, ?, ?, ?, ?, ?)", [datosCurso.Titulo, datosCurso.Descripcion,
+                        datosCurso.Localidad, datosCurso.Direccion, datosCurso.NumPlazas, datosCurso.FechaInicio, datosCurso.FechaFin],
+            function(err, rows) { 
+                con.release();                
+                if (err) {
+                    callback(err);
+                } else {
+                    datosCurso.IdCurso = rows.insertId;
+                    insertHorariosCurso(datosCurso, callback);
+                }                
+            });
+        }
+    });
+}
+
+function insertHorariosCurso(datosCurso, callback){
+    if(datosCurso.Horarios.length === 0) callback(datosCurso.IdCurso);
+    datosCurso.Horarios.forEach(function(h, indexH, arrayH){
+        pool.getConnection(function(err, con) {
+        if (err) {
+            callback(err);
+        } else {
+            con.query("INSERT INTO Horarios(IdCurso, Dia, HoraInicio, HoraFin)" + 
+                           " VALUES (?, ?, ?, ?)", [datosCurso.IdCurso, h.Dia,
+                            h.HoraInicio, h.HoraFin],
+                function(err, rows) { 
+                    con.release();                
+                    if (err) {
+                        callback(err);
+                    } else {
+                        if(indexH === arrayH.length - 1){
+                            callback(null, datosCurso.IdCurso);
+                        }                        
+                    }                
+                });
+            }
+        });
+    });
+}
+
+module.exports = {
+    insertCurso: insertCurso
+};
--- app.js
+++ app.js
@@ -2,10 +2,61 @@
  * GRUPO 111 - Rubén Casado y Juan José Prieto
  */
 var express = require("express");
+var bodyParser = require("body-parser");
 var DAO = require('./DAO.js');
 var config = require('./config.js');
 
 var app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", function(req, res){
+    /* curso = {};
+    curso.Titulo = "AAA";
+    curso.Descripcion = "BBB";
+    curso.Localidad = "MMM";
+    curso.Direccion = "MMO";
+    curso.NumPlazas = 5;
+    curso.FechaInicio = new Date();
+    curso.FechaFin = new Date();
+    curso.Horarios = [];
+    var horario = {};
+    horario.Dia = "Lunes";
+    var date = new Date();
+    horario.HoraInicio = date.getTime();
+    horario.HoraFin = date.getTime();
+    curso.Horarios.push(horario);
+    var horario = {};
+    horario.Dia = "Martes";
+    var date = new Date();
+    horario.HoraInicio = date.getTime();
+    horario.HoraFin = date.getTime();
+    curso.Horarios.push(horario);
+    var horario = {};
+    horario.Dia = "Miercoles";
+    var date = new Date();
+    horario.HoraInicio = date.getTime();
+    horario.HoraFin = date.getTime();
+    curso.Horarios.push(horario); 
+   DAO.insertCurso(curso, function(err, datos){
+       res.end();
+   }) ; */
+});
+
+app.post("/cursos", function(req, res) {
+    DAO.insertCurso(req.body, function(err, id){
+        if(err){
+           res.status(500);
+        } 
+        else{
+           var r = {};
+           r.IdCursoInsertado = id;
+           res.status(201);
+           res.json(r);
+        }      
+        res.end();
+    });
+});
 
 app.listen(config.port, function(next) {
     console.log("Servidor arrancado en el puerto " + config.port);
