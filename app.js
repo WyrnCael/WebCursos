b65c1cbec270bffb47d02edfb685267d49d3c66e
WyrnCael <juanjpri@ucm.es>
2017-01-31 21:03:35 +0100

    Certificado SSL

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
  * GRUPO 111 - Rubén Casado y Juan José Prieto
  */
 var express = require("express");
-var path = require("path")
+var path = require("path");
+var https = require("https");
+var fs = require("fs");
 var config = require('./config.js');
 var routerCursos = require("./routerCursos");
 var routerUsuarios = require("./routerUsuarios");
+var clavePrivada = fs.readFileSync(config.private_key);
+var certificado = fs.readFileSync(config.certificate);
 
 var app = express();
 app.use("/cursos", routerCursos);
@@ -19,6 +23,9 @@ app.get("/", function(req, res){
     res.end();
 });
 
-app.listen(config.port, function(next) {
+var servidor = https.createServer(
+ { key: clavePrivada, cert: certificado }, app );
+
+servidor.listen(config.port, function(next) {
     console.log("Servidor arrancado en el puerto " + config.port);
 });
